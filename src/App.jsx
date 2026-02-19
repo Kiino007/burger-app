890ccde38e027d58ff08f147c7adc613fb827d4f
Kiino007 <149212991+Kiino007@users.noreply.github.com>
2026-02-19 15:44:57 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl text-yellow-400 font-bold">
+        Burger App funcionando 🍔
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
